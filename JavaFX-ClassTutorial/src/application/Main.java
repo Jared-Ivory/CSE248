b9a1dcf57d4092aee5c6c744f2c32566d7e95d75
Jared Ivory <ivorj32@mail.sunysuffolk.edu>
2018-09-19 12:44:15 -0400

    Signed-off-by: Jared Ivory <ivorj32@mail.sunysuffolk.edu>

---
+++ JavaFX-ClassTutorial/src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Classtut.fxml"));
+			Scene scene = new Scene(root, 300, 400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
